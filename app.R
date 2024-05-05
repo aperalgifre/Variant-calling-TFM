9e8c3dcc3ca8f69545dcc5de300c13285a09e77c
Arnau Peral Gifre <a.peralgifre@gmail.com>
2024-05-05 21:39:37 +0200

    Upload version 5/5

--- app.R
+++ app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(VariantAnnotation)
 library(data.table)
 library(shinyWidgets)
+library(RTCGA)
 
 # Set maximum upload size
 options(shiny.maxRequestSize = 500*1024^2) # Set maximum upload size to 500 MB
@@ -16,6 +17,7 @@ ui <- fluidPage(
       uiOutput("variant_type_selector"),
       uiOutput("pathogenicity_key_selector"),
       uiOutput("pathogenicity_category_selector"),
+      uiOutput("cancer_type_selector"),  # New select input for TCGA cancer type
       actionButton("run_analysis", "Run analysis"),
       downloadButton("download_csv", "Download Filtered Variants CSV")
     ),
@@ -38,6 +40,9 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output, session) {
+  # Define a reactive value for tcga_data
+  tcga_data <- reactiveVal(NULL)
+  
   # Initialize reactive values to store INFO keys
   allele_freq_keys <- reactiveVal(character(0))
   variant_type_keys <- reactiveVal(character(0))
@@ -79,6 +84,13 @@ server <- function(input, output, session) {
         shiny::selectInput("pathogenic_key", "Select Pathogenicity Key:",
                            choices = c("", pathogenic_keys()), selected = "")
       })
+      
+      output$cancer_type_selector <- renderUI({
+        selectInput("cancer_type", "Select TCGA Cancer Type:",
+                    choices = c("ACC","BLCA", "BRCA", "CESC", "CHOL", "COAD", "COADREAD", "DLBC", "ESCA", "GBMLGG", "GBM", "HNSC", "KICH", 
+                                "KIPAN", "KIRC", "KIRP", "LAML", "LGG", "LIHC", "LUAD", "LUSC", "OV", "PAAD", "PCPG", "PRAD", "READ", "SARC",
+                                "SKCM", "STAD", "STES", "TGCT", "THCA", "UCEC", "UCS", "UVM"), selected = "BRCA")
+      })
     }
   })
   
@@ -103,49 +115,6 @@ server <- function(input, output, session) {
     }
   })
   
-  # Define a reactive expression to filter variants and return the filtered dataframe
-  filtered_variants <- reactive({
-    req(input$vcf_file, input$pathogenic_key, input$pathogenic_categories)
-    
-    # Read VCF file
-    vcf <- readVcf(input$vcf_file$datapath, "hg19")
-    
-    # Filter pathogenic variants based on selected key and categories
-    pathogenic_variants <- tryCatch({
-      clnsig_values <- info(vcf)[[input$pathogenic_key]]
-      subset(vcf, clnsig_values %in% input$pathogenic_categories)
-    }, error = function(e) {
-      return(data.frame())
-    })
-    
-    # Process the filtered variants
-    chrom <- as.data.frame(seqnames(rowRanges(pathogenic_variants)))
-    ranges <- (as.data.frame(ranges(rowRanges(pathogenic_variants)))[, -c(2,3)])
-    ref <- as.data.frame(ref(pathogenic_variants))
-    alt <- as.data.frame(alt(pathogenic_variants))[, -c(1,2)]
-    qual <- as.data.frame(qual(pathogenic_variants))
-    filter <- as.data.frame(filt(pathogenic_variants))
-    info <- as.data.frame(info(pathogenic_variants))
-    
-    partial_df <- cbind(chrom, ranges, ref, alt, qual, filter)
-    colnames(partial_df) <- c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER")
-    completed_df <- cbind(partial_df, info)
-    rownames(completed_df) <- NULL
-    
-    return(completed_df)
-  })
-  
-  # Render the download button
-  output$download_csv <- downloadHandler(
-    filename = function() {
-      paste0("filtered_variants", ".csv")
-    },
-    content = function(file) {
-      # Write filtered dataframe to CSV file
-      fwrite(filtered_variants(), file, row.names = FALSE)
-    }
-  )
-  
   # Function to perform analysis
   analyze_vcf <- function(vcf, allele_freq_keys, variant_type_key, pathogenic_key, pathogenic_categories) {
     # Variant counts
@@ -163,7 +132,7 @@ server <- function(input, output, session) {
     # Filter pathogenic variants based on selected categories
     pathogenic_variants <- tryCatch({
       clnsig_values <- info(vcf)[[pathogenic_key]]
-      subset(vcf, clnsig_values %in% pathogenic_categories)
+      subset(vcf, clnsig_values %in% input$pathogenic_categories)
     }, error = function(e) {
       return(data.frame())
     })
@@ -213,6 +182,75 @@ server <- function(input, output, session) {
       }
     })
   }
+  
+  # Define a reactive expression to filter variants and return the filtered dataframe
+  filtered_variants <- reactive({
+    req(input$vcf_file, input$pathogenic_key, input$pathogenic_categories)
+    
+    # Read VCF file
+    vcf <- readVcf(input$vcf_file$datapath, "hg19")
+    
+    # Filter pathogenic variants based on selected key and categories
+    pathogenic_variants <- tryCatch({
+      clnsig_values <- info(vcf)[[input$pathogenic_key]]
+      subset(vcf, clnsig_values %in% input$pathogenic_categories)
+    }, error = function(e) {
+      return(data.frame())
+    })
+    
+    # Process the filtered variants
+    chrom <- as.data.frame(seqnames(rowRanges(pathogenic_variants)))
+    ranges <- (as.data.frame(ranges(rowRanges(pathogenic_variants)))[, -c(2,3)])
+    ref <- as.data.frame(ref(pathogenic_variants))
+    alt <- as.data.frame(alt(pathogenic_variants))[, -c(1,2)]
+    qual <- as.data.frame(qual(pathogenic_variants))
+    filter <- as.data.frame(filt(pathogenic_variants))
+    info <- as.data.frame(info(pathogenic_variants))
+    
+    partial_df <- cbind(chrom, ranges, ref, alt, qual, filter)
+    colnames(partial_df) <- c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER")
+    completed_df <- cbind(partial_df, info)
+    rownames(completed_df) <- NULL
+    
+    return(completed_df)
+  })
+  
+  # Function to download TCGA data and generate CSV
+  output$download_csv <- downloadHandler(
+    filename = function() {
+      paste0("filtered_variants", ".csv")
+    },
+    content = function(file) {
+      req(input$cancer_type)
+      
+      # Download TCGA data
+      temp_dir <- tempdir()
+      downloadTCGA(input$cancer_type, dataSet = 'Mutation_Packager_Calls.Level', destDir = temp_dir)
+      
+      # Construct file path for downloaded data
+      data_folder <- file.path(temp_dir, paste0("gdac.broadinstitute.org_", input$cancer_type, ".Mutation_Packager_Calls.Level_3.2016012800.0.0"))
+      
+      # Read the data 
+      data <- readTCGA(data_folder, 'mutations')  
+      
+      # Get filtered variants dataframe
+      filtered_df <- filtered_variants()
+      
+      # Compare with TCGA data and add extra column
+      if (!is.null(data)) {
+        filtered_df$Present_in_TCGA <- ifelse(apply(filtered_df, 1, function(row) {
+          any(apply(data, 1, function(tcga_row) {
+            paste(row["CHROM"], row["POS"], row["REF"]) == paste(tcga_row["Chromosome"], tcga_row["Start_Position"], tcga_row["Reference_Allele"])
+          }))
+        }), "Yes", "No")
+      } else {
+        filtered_df$Present_in_TCGA <- NA
+      }
+      
+      # Write filtered dataframe to CSV file
+      fwrite(filtered_df, file, row.names = FALSE)
+    }
+  )
 }
 
 # Run the application
